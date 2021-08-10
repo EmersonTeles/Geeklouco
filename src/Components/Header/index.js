0649b3e3b475c3e1af64ff69fbc48ef56404c033
EmersonTeles <emersonteles21@gmail.com>
2021-08-10 16:38:22 -0300

    alterando layout do header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,14 +1,17 @@
 import './styles.css'
 import { Link } from 'react-router-dom'
-
+import search_icon from '../../Assets/search-icon.svg'
 import user_icon from '../../Assets/user-icon.svg'
 
 export default function Header (){
     return(
-        <nav className="navbar"> 
+        <header className="header"> 
             <div className="header_upper">
               <Link to="/"><h1>geeklouco.com</h1></Link>
-              <input type="text" placeholder="Herói? Vilão? O que você imaginar aqui..."/>
+              <form>
+                <input type="text" placeholder="Herói? Vilão? O que você imaginar aqui..."/>
+                <button><img src={search_icon} alt="botão de pesquisar"/></button>
+              </form>
               <Link className="login" to="/login">
                 <button>
                   <h3>Login</h3>
@@ -16,15 +19,17 @@ export default function Header (){
                 </button>
               </Link>
             </div>
-            <ul>
-              <li><button>Universo Marvel</button></li>
-              
-              <li><button>Universo DC</button></li>
-              
-              <li><button>Star Wars</button></li>
-              
-              <li><button>Vilões</button></li>
-            </ul>
-          </nav>
+            <nav>
+              <ul>
+                <li><button>Universo Marvel</button></li>
+                
+                <li><button>Universo DC</button></li>
+                
+                <li><button>Star Wars</button></li>
+                
+                <li><button>Vilões</button></li>
+              </ul>
+            </nav>
+          </header>
     )
 }
