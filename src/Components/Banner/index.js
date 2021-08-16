f0de0efbde59ce63ac5bed67f58bcf4670afa04f
EmersonTeles <emersonteles21@gmail.com>
2021-08-16 17:12:24 -0300

    .

--- src/Components/Banner/index.js
+++ src/Components/Banner/index.js
@@ -1,12 +1,12 @@
 import './styles.css'
 import img_banner1 from '../../Assets/thumb1.jpg';
-import img_banner2 from '../../Assets/thumb2.jpg';
-import img_banner3 from '../../Assets/thumb3.jpg';
 
 export default function Banner(){
     return(
         <div className="banner">
-            <div className="foto"/>
+            <div className="carousel">
+                <img className="foto" src={img_banner1} alt="Capa do Flash"/>x
+            </div>
             <div className="head-line">
                 <h1>AQUI VOCÊ ENCONTRA TUDO SOBRE SEUS HERÓIS E VILÕES FAVORITOS!</h1>
             </div>
