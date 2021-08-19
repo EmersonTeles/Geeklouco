ccf94be9004c8c28a1855f51647a8540aef32729
EmersonTeles <emersonteles21@gmail.com>
2021-08-19 09:42:38 -0300

    revertendo banner

--- src/Components/Banner/index.js
+++ src/Components/Banner/index.js
@@ -1,12 +1,9 @@
 import './styles.css'
-import img_banner1 from '../../Assets/thumb1.jpg';
 
 export default function Banner(){
     return(
         <div className="banner">
-            <div className="carousel">
-                <img className="foto" src={img_banner1} alt="Capa do Flash"/>x
-            </div>
+            <div className="foto"/>
             <div className="head-line">
                 <h1>AQUI VOCÊ ENCONTRA TUDO SOBRE SEUS HERÓIS E VILÕES FAVORITOS!</h1>
             </div>
